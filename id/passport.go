6453acf31192b247f26ca3d41508493cf6f143a7
Gleb Karpushkin <gleb.karp@gmail.com>
2022-04-08 14:31:01 +0300

    Fixed Passports Data

--- id/passport.go
+++ id/passport.go
@@ -1,10 +1,12 @@
 package id
 
 type Passport struct {
-	country     string
-	cityOfIssue string
-	issuer      string
-	dateOfIssue string
-	number      string
-	photo       []byte //base64
+	country         string
+	cityOfIssue     string
+	issuer          string
+	dateOfIssue     int64
+	dateOfDismissal int64
+	ordinatory      bool
+	number          string
+	photo           []byte //base64
 }
